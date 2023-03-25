19c6d87034b173bd414813efd8fc005e9e189b95
smiadv2022 <msorokina@spacemedia.com.ua>
2023-03-25 02:44:54 +0200

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const divRefs = document.querySelector('.gallery');
+function createGalleryList(items) {
+  return items
+    .map(
+      item => `<li class="gallery__item">
+     <a class="allery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}"
+     data-source="${item.original}"
+     alt="${item.description}" />
+     </a>
+     </li>`
+    )
+    .join('');
+}
+const addGalleryList = createGalleryList (galleryItems);
+divRefs.innerHTML = addGalleryList;
+divRefs.addEventListener ("click", onImgFocus);
+function onImgFocus(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src ="${event.target.dataset.source}" width="800" height="600" >
+  `);
+  instance.show();
+  divRefs.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const divRefs = document.querySelector('.gallery');
+function createGalleryList(items) {
+  return items
+    .map(
+      item => `<li class="gallery__item">
+     <a class="allery__link" href="${item.original}">
+     <img class="gallery__image"  src="${item.preview}"
+     alt="${item.description}" />
+     </a>
+     </li>`
+    )
+    .join('');
+}
+const addGalleryList = createGalleryList(galleryItems);
+divRefs.innerHTML = addGalleryList;
+divRefs.addEventListener('click', onImgFocus);
+function onImgFocus(event) {
+  event.preventDefault();
+
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox');
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
